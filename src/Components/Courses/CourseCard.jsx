386ecad1cc66136a78649b1b65e7ff933c78165e
callmeVineeth <vineethpv024@gmail.com>
2024-03-15 13:48:52 +0530

    bug fixed with data mapping in the course card

--- src/Components/Courses/CourseCard.jsx
+++ src/Components/Courses/CourseCard.jsx
@@ -1,38 +1,54 @@
-import React from 'react'
+import React from "react";
+// import getImageUrl from "../../CustomComponents/ImagePath";
 
+function CourseCard({ data, img, CourseTitle }) {
+  // const imageUrl = getImageUrl(data.img);
 
-function CourseCard({data,img,CourseTitle}) {
-  // p-2  justify-evenly gap-2 py-14 md:px-16 flex flex-row flex-wrap md:gap-12
-  // bg-stone-200 shadow-xl w-48 my-2 md:w-56  h-auto md:h-auto -z-10
-  //         transition-all duration-300 hover:scale-105  flex flex-col items-center
-  
   return (
- 
     <>
-      <div className='w-full coursePage flex-col justify-center items-center py-20'>
-        <div className=' text-center font-semibold text-xl md:text-4xl p-6 uppercase
-        bg-gradient-to-r from-pink-700 to-blue-500 text-transparent bg-clip-text  '>{CourseTitle} </div>
-        <div className='grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2   gap-5 md:gap-10 mt-7 p-5'>
-    
-          {data.map((item,key)=>(
-            <div className=' bg-stone-200 shadow-xl max-w-[380px] mx-auto'>
-                <img src={item.img ? item.img :img} alt='courcePic' className=""/>
-                  <h1 className=' text-center font-semibold text-lg md:text-xl md:leading-tight'>{item.course}</h1>
-                <div className=' flex flex-col justify-stretch text-sm md:text-md p-2 md:p-4'>
-                      <span> {item.point1}. </span>
-                      <span> {item.point2}.</span>
-                      <span> {item.point3}.</span>
-                      <span className=' md:text-lg font-semibold cursor-pointer'> Tution Fees: {item.Price} </span>
-                      <span className=' md:text-lg font-semibold cursor-pointer'> Time Duration :{item.Duration} </span>
-                </div>
+      <div className="w-full coursePage flex-col justify-center items-center py-20">
+        <div
+          className=" text-center font-semibold text-xl md:text-4xl p-6 uppercase
+        bg-gradient-to-r from-pink-700 to-blue-500 text-transparent bg-clip-text  "
+        >
+          {CourseTitle}{" "}
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2   gap-5 md:gap-10 mt-7 p-5">
+          {data.map((item, id) => (
+            <div
+              className=" bg-stone-200 shadow-xl max-w-[380px] mx-auto"
+              key={id}
+            >
+              <img
+                src={
+                  // Concise conditional logic for image source
+                  item.img ? item.img : img
+                }
+                alt="courcePic"
+                className=""
+              />
+              <h1 className=" text-center font-semibold text-lg md:text-xl md:leading-tight">
+                {item.course}
+              </h1>
+              <div className=" flex flex-col justify-stretch text-sm md:text-md p-2 md:p-4">
+                <span> {item.point1}. </span>
+                <span> {item.point2}.</span>
+                <span> {item.point3}.</span>
+                <span className=" md:text-lg font-semibold cursor-pointer">
+                  {" "}
+                  Tution Fees: {item.Price}{" "}
+                </span>
+                <span className=" md:text-lg font-semibold cursor-pointer">
+                  {" "}
+                  Time Duration :{item.Duration}{" "}
+                </span>
+              </div>
             </div>
-          ))
-          }
-    
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default CourseCard
+export default CourseCard;
